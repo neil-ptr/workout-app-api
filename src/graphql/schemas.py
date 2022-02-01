da18c0361c92e7e4fc61193a1cb48336b1dbbd23
neilZon <neilcviloria@gmail.com>
2022-02-01 01:24:36 -0700

    login schemas

--- src/graphql/schemas.py
+++ src/graphql/schemas.py
@@ -1,8 +1,6 @@
 import strawberry
 from typing import List, Optional
 
-from src.database import crud
-from src.database.crud import exercises, workouts
 from src.graphql.resolvers import get_exercise_templates, get_exercises, get_sets, get_exercise_template, get_workout_template
 
 @strawberry.type
@@ -44,26 +42,21 @@ class User:
     id: int
     email: str
     firstname: str
-    workouts: List[Workout]
-    workoutTemplates: List[WorkoutTemplate]
+    workouts: Optional[List[Workout]] = None
+    workoutTemplates: Optional[List[WorkoutTemplate]] = None
 
 @strawberry.type
-class Query:
-    @strawberry.field
-    def user(self, id: strawberry.ID) -> User:
-        return crud.get_user(id)
+class Tokens:
+    refreshToken: str
+    accessToken: str
 
-    @strawberry.field
-    def workout(self, active: Optional[bool] = False, before: Optional[str] = None, after: Optional[str] = None) -> List[Workout]:
-        workouts = crud.get_workouts(6, active=active, before=before, after=after)
-        return workouts
+@strawberry.type
+class LoginSuccess:
+    tokens: Tokens
 
-    @strawberry.field
-    def workoutTemplate(self, user_id: int) -> List[WorkoutTemplate]:
-        workout_template = crud.get_workout_templates(user_id)
-        return workout_template
+@strawberry.type
+class LoginError:
+    message: str
 
-    @strawberry.field
-    def exercises(self, workout_id: int) -> List[Exercise]:
-        exercises = crud.get_exercises(workout_id)
-        return exercises
+LoginResult = strawberry.union("LoginResult", (LoginSuccess, LoginError))
+
